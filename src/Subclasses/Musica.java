97f0056caf37c4cbbe931369a450c08175bdd19d
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-08 17:50:35 -0300

    subclass variables and constructor

--- src/Subclasses/Musica.java
+++ src/Subclasses/Musica.java
@@ -2,5 +2,14 @@ package Subclasses;
 import Superclasse.Audio;
 
 public class Musica extends Audio {
+    //Mother class constructor
+    public Musica(String titulo, int duracaoEmSegundos, int totalDeReproducoes, int curtidas, double classificacao) {
+        super(titulo, duracaoEmSegundos, totalDeReproducoes, curtidas, classificacao);
+    }
+
+    //Specific variables
+    private String album;
+    private String cantor;
+    private String genero;
 
 }
