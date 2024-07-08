212e398c0ecab983a88577e7650051dd0b39f2c4
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-08 17:52:44 -0300

    Podcast subclass variables and constructor

--- src/Subclasses/Podcast.java
+++ src/Subclasses/Podcast.java
@@ -2,5 +2,13 @@ package Subclasses;
 import Superclasse.Audio;
 
 public class Podcast extends Audio {
+    //Mother class constructor
+    public Podcast(String titulo, int duracaoEmSegundos, int totalDeReproducoes, int curtidas, double classificacao) {
+        super(titulo, duracaoEmSegundos, totalDeReproducoes, curtidas, classificacao);
+    }
+
+    //Specific variables
+    private String apresentador;
+    private String descricao;
 
 }
